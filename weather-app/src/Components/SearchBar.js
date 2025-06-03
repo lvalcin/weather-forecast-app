7d3a37a7ddc027b70b39941dea34e0f5a1fb1d1e
Lovnyth Valcin <lovnyth@Mac.attlocal.net>
2025-06-03 04:55:48 -0400

    successful fetch of weather data from api

--- weather-app/src/Components/SearchBar.js
+++ weather-app/src/Components/SearchBar.js
@@ -4,12 +4,37 @@ import React, {useState} from "react";
 const SearchBar = ()=> {
 
 const [city,setCity]= useState("")
+const apiKey = process.env.REACT_APP_WEATHER_API_KEY
+// const apiUrl = process.env.REACT_APP_WEATHER_API_URL
+
+
+const getWeather=()=>{
+  const weatherUrl = `https://weather-api167.p.rapidapi.com/api/weather/forecast?place=${encodeURIComponent(city)}&cnt=3&units=standard&type=three_hour&mode=json&lang=en`
+  
+  fetch(weatherUrl, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "x-rapidapi-host": "weather-api167.p.rapidapi.com",
+      "x-rapidapi-key": apiKey 
+    }
+  })
+    .then((response)=>{
+      return response.json()
+    })
+    .then((data)=>{
+      console.log(data, "HERE IS THE DATA !!!!")
+    })
+}
+
+
 
   return (
     <div className="container-fluid d-flex justify-content-center">
         <form onSubmit={(e)=>{
           e.preventDefault();
           console.log(city, "HERE IS THE CITY!!!!")
+          getWeather();
         }}>
             <input className="form-control mb-3"
             type="text" 
