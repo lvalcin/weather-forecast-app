7a4f361881cc1e39237f610e7918d3fd1e993233
Lovnyth Valcin <lovnyth@Mac.attlocal.net>
2025-06-03 06:04:36 -0400

    temperature converted from K to display in F

--- weather-app/src/Components/WeatherCard.js
+++ weather-app/src/Components/WeatherCard.js
@@ -1,16 +1,22 @@
 import React, {useState} from "react";
 
 const WeatherCard = ({data}) =>{
-      if (!data || !data.city){
+    if (!data || !data.city){
         return <div>No weather data to display.</div>;
       }
+
+    const tempInKelvin = data.list[0].main.temprature;
+    const tempInFahrenheit = ((tempInKelvin - 273.15) * 9/5 + 32).toFixed(1); //converst temp from Kelvin to Fahrenheit
+
     return(
         <div>
             <div className="card" style={{width: "18rem"}}>
             {/* <img src="..." class="card-img-top" alt="..."> */}
             <div className="card-body">
-                <h5 className="card-title">{data.city.name}</h5>
-                <p className="card-text">Temperature: {data.list[0].main.temprature}</p>
+                <h1 className="fw-bold card-title">{data.city.name}</h1>
+                <p className="card-text">Temperature: {tempInFahrenheit}°F</p>
+                <p className="card-text">Description: {data.list[0].weather[0].description} </p>
+                <p className="cad-text">Summary:{data.list[0].summery} </p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
             </div>
